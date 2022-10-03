2f18635280206e917aef55807062b72ddcc5b6bb
Ramon Hernandez Leon <100383351@alumnos.uc3m.es>
2022-10-03 20:40:34 +0200

    Cleaning up local repo

--- README.md
+++ README.md
@@ -1,2 +1,213 @@
-# anti_dusty_miner
-HW model and SW implementation of a Real Time System to control an intelligent wagon that moves along a mine.
+# *Dusty miner*
+
+### Contributors <!-- omit in toc -->
+- [Alberto Huertas](https://github.com/aHuertasR)
+- [Ramón Hernandez](https://github.com/uRHL)
+
+### Sections <!-- omit in toc -->
+
+- [*Dusty miner*](#dusty-miner)
+	- [Objective](#objective)
+	- [Materials](#materials)
+	- [Functional specifications](#functional-specifications)
+		- [Speed control](#speed-control)
+		- [Concrete mixer control](#concrete-mixer-control)
+		- [Lamp control](#lamp-control)
+		- [I/O control](#io-control)
+		- [Loading point control](#loading-point-control)
+		- [Emergency mode control](#emergency-mode-control)
+	- [Sensors](#sensors)
+	- [Actuators](#actuators)
+	- [Execution modes - Software module](#execution-modes---software-module)
+			- [Normal mode](#normal-mode)
+			- [Braking mode](#braking-mode)
+			- [Stop mode (SW)](#stop-mode-sw)
+			- [Emergency mode (SW)](#emergency-mode-sw)
+	- [Execution modes - Hardware module](#execution-modes---hardware-module)
+			- [Distance selection mode](#distance-selection-mode)
+			- [Approach mode](#approach-mode)
+			- [Stop mode (HW)](#stop-mode-hw)
+			- [Emergency mode (HW)](#emergency-mode-hw)
+	- [Real-time formalization](#real-time-formalization)
+		- [Software module](#software-module)
+		- [Hardware module](#hardware-module)
+
+---
+
+## Objective
+
+Build a real time system for a wagon so that it can carry safely concrete through a railway. It must be able to:
+
+- maintain the speed within the safe range.
+- mix the concrete so that it is not solidified.
+- turn on/off the lamps during dark zones (tunnels)
+- stop the wagon in the corresponding loading points (deposits)
+- stop the wagon on emergencies.
+
+## Materials
+
+Software: Raspberry Pi (RTMS OS)
+
+Hardware: Arduino moderboard
+
+## Functional specifications
+
+### Speed control
+
+The environment (railway) modifies the speed of the wagon
+* Flat slope: do not affect the speed
+* Up slope: Constant speed reduction (-0.25 m/s2)
+* Down slope: Constant acceleration (0.25 m/s2)
+
+### Concrete mixer control
+
+- If the concrete is not mixed for 60 seconds it starts to solidify.
+- Additionally, the concrete requires to be mixed for at least 30 seconds.
+- If the state of the mixer had not change in the last 30 seconds modify the state of the mixer.
+- The mixer is overcharged (broken) if it is working for more than 60 seconds consecutively.
+- The mixer requires 30 seconds to rest between each swith (on/off)
+
+### Lamp control
+
+- The lamps should be turned on/off in less that 12 seconds after getting in/out of the tunnel
+
+### I/O control
+
+- Messaging the hardware module and wait for the answer (400 ms)
+- Show the new state on the display (display functions) (500 ms)
+- Total cycle duration is 900 ms
+
+### Loading point control
+
+### Emergency mode control
+
+
+## Sensors
+- **Speed sensor**: returns the current speed of the wagon
+- **Slope sensor**: returns the current slope
+- **Light sensor**: detects the intensity of the environmental light and indicates if the lights should be turned on or not
+- **Distance sensor**: returns the remaining distance between the current point and the next station
+- **Loading sensor**: detects if the wagon is still being loaded (the wagon cannot move) or if it has finished (the wagon can continue)
+
+
+## Actuators
+- **Accelerator (on/off)**: Applies a constant acceleration (0.5 m/s2)
+- **Brake (on/off)**: Applies a constant speed reduction (-0.5 m/s2)
+- **Mixer (on/off)**: turns on/off the mixer's engine
+- **Lamps (on/off)**
+
+---
+
+## Execution modes - Software module
+
+#### Normal mode
+
+- Safe speed range: 40m/s – 70m/s
+- The distance until the next stop is checked periodically. When the *safe braking distance* is reached, the wagon enters in *braking mode* (normal mode -> braking mode)
+- *Safe braking distance*: minimum distance required to stop safely the wagon in the next station. Consider always the worst case scenario
+
+#### Braking mode
+
+- Wagon speed < 10 m/s
+- Lamps always on
+- The tasks speed, acceleration and brake control have their period reduced to the half they have while in normal mode
+- When the distance until the station is 0 meters, and the rest of requirements are met, the wagon enters in stop mode (braking mode -> stop mode)
+
+#### Stop mode (SW)
+
+- The wagon is stopped (speed 0 m/s) until the *loading sensor* indicates that the loading process has finished (stop mode -> normal mode)
+- Lamps always on
+- The mixer should keep working
+
+#### Emergency mode (SW)
+
+Whenever there is a failure (e.g. timing failure), regardless of the operation mode, the wagon enters in emergency mode. When this happens:
+
+- The software module notifies the hardware module to enter in emergency mode
+- The wagon is stopped completely (0 m/s)
+- The lamps are always turned on
+- The mixer keeps working
+
+
+## Execution modes - Hardware module
+
+#### Distance selection mode
+
+- The wagon periodicaly updates the distance to the next station
+- The wagon compares the current distance to the next station and the *safe braking distance* to decide if the operation mode should change from *normal* to *approaching*.
+- The display shows the distance to the next station 
+
+#### Approach mode
+
+- The wagon compares the current distance to the next station. When it reaches 0, the wagon transitions from *approaching mode* to *stop mode*.
+- The display shows the distance remaining until the station
+
+#### Stop mode (HW)
+
+- The displays shows the distance until the next station (0 meters)
+- Checks the value of the *loading sensor* to decide if the operation mode should change from *loading* to *normal*.
+
+#### Emergency mode (HW)
+The harware module will switch to emergency mode when the sofware
+module request it.
+
+---
+
+
+## Real-time formalization
+
+#### Symbol table <!-- omit in toc -->
+
+| Symbol | Meaning |
+|:---:|:---:|
+| (\*) | Recommended value, but it can be modified |
+| X | Task not executed |
+| T/D | Task period or deadline |
+| C | Task execution duration |
+
+### Software module
+
+Notes
+
+- Time is measured in seconds
+- Ci = 0.9 for all tasks
+
+|  | Normal mode | Braking mode | stop mode | Emergency mode |
+|:---:|:---:|:---:|:---:|:---:|
+|  |  |  |  |  |
+| Task | T/D | T/D | T/D | T/D |
+| Read slope | 10 | 10(\*) | X | 10 |
+| Read speed | 10 | 5 | X | 10 |
+| Read distance | 10 | 10(\*) | X | X |
+| Turn on/off accelerator | 10 | 5 | X | 10 |
+| Turn on/off brake | 10 | 5 | X | 10 |
+| Turn on/off mixer | 15 | 15 | 15 | 15 |
+| Read light sensor | 6 | X | X | X |
+| Turn on/off lamps | 6 | 30(\*) | 5(\*) | 6 |
+| Read loading sensor | X | X | 5(\*) | 10(\*) |
+| Activate the emergency mode (no return) | X | X | X | 10(\*) |
+
+
+### Hardware module
+
+Notes
+
+- Time is measured in ms
+- Once the Arduino circuit is built, the real duration of each task must be measured manually several times. The final value used for Ci will be the worst one among all the measures. 
+
+|  | Distance selection mode | Approaching mode | stop mode | Emergency mode |
+|:----:|:----:|:----:|:----:|:----:|
+|  |  |  |  |  |
+| Task T/D C | T/D | T/D | T/D | T/D |
+| On/Off accelerator | 200(\*) | 200(\*) | 200(\*) | 200(\*) |
+| On/Off brake | 200(\*) | 200(\*) | 200(\*) | 200(\*) |
+| On/Off Mixer | 200(\*) | 200(\*) | 200(\*) | 200(\*) |
+| Compute and show speed | 200(\*) | 200(\*) | 200(\*) | 200(\*) |
+| Read slope | 200(\*) | 200(\*) | 200(\*) | 200(\*) |
+| Read light sensor | 200(\*) | 200(\*) | 200(\*) | X |
+| Turn on/off lamps | 200(\*) | 200(\*) | 200(\*) | 200(\*) |
+| Distance selection | 200(\*) | X | X | X |
+| Display distance | 200(\*) | 200(\*) | X | X |
+| Validate distance | 200(\*) | X | X | X |
+| Read end of slope | X | X | 200(\*) | X |
+
---
+++ src/arduino_codeA.ino
@@ -0,0 +1,438 @@
+// --------------------------------------
+// Includes
+// --------------------------------------
+#include <string.h>
+#include <stdio.h>
+#include <Wire.h>
+
+// --------------------------------------
+// Global Constants
+// --------------------------------------
+
+//Main cycle = 1 sec
+#define MAIN_CYCLE 0.001
+//Number of Secondary Cycles within one Main Cycle
+#define NUM_SC 5
+//In micro seconds
+#define SC_TIME 200
+//Comunication server constants
+#define PORT 9000
+#define MESSAGE_SIZE 8
+#define SLAVE_ADDR 0x8
+//Messages
+#define ERROR_MSG "MSG: ERR\n"
+#define OK_MSG "%s  OK\n>"
+#define UP_SLP_MSG "SLP:  UP\n"
+#define FLAT_SLP_MSG "SLP:FLAT\n"
+#define DOWN_SLP_MSG "SLP:DOWN\n>"
+#define SPD_MSG "SPD:%s\n"
+#define SET 1
+#define CLR 0
+//Pin numbers
+#define GAS_PIN_OUT 13
+#define BRK_PIN_OUT 12
+#define MIX_PIN_OUT 11
+#define SPD_PIN_OUT 10
+#define SLP_PIN_IN_1 9
+#define SLP_PIN_IN_2 8
+
+//Units m/s^2
+#define GAS_POWER 0.5
+#define BRAKE_POWER 0.5
+#define INERTIA 0.25
+//Units: m/s
+#define MAX_SPEED 70
+#define MIN_SPEED 40
+
+// --------------------------------------
+// Global Variables
+// --------------------------------------
+
+//0 --> off
+//1 --> on
+int mixer_status = 0;
+int gas_status = 0;
+int brake_status = 0;
+// Admissible values = -1(downwards), 0(flat), 1(upwards)
+int slope = 0;
+//cannot be negative
+float speed = 0.0;
+//Auxiliary variables for the messaging service
+bool request_received = false;
+bool answer_requested = false;
+char request [MESSAGE_SIZE + 1];
+char response [MESSAGE_SIZE + 1];
+int count = 0;//Aux variable to count the number of characters red from input
+int sc = 0;//Counter for the secondary cycles elapsed since the beginning of the execution
+unsigned long lastCall = 0;//Auxiliary timer variable
+
+void setup()  
+{
+  //Set up all the pins
+  pinMode(GAS_PIN_OUT, OUTPUT);
+  pinMode(BRK_PIN_OUT, OUTPUT);
+  pinMode(MIX_PIN_OUT, OUTPUT);
+  pinMode(SPD_PIN_OUT, OUTPUT);
+  pinMode(SLP_PIN_IN_1, INPUT);
+  pinMode(SLP_PIN_IN_2, INPUT);
+    
+  // Initialize I2C communications as a worker
+  Wire.begin(SLAVE_ADDR);
+  
+  // Function to run when data requested from master
+  Wire.onRequest(requestEvent);
+  
+  // Function to run when data received from master
+  Wire.onReceive(receiveEvent);
+  
+  Serial.begin(PORT);
+  
+}
+
+  
+
+void loop()
+{
+  unsigned long start;
+  unsigned long end;
+  switch(sc%NUM_SC){
+   
+    case 0:    
+    start = micros();
+    slope_request();
+    end = micros();
+    delayMicroseconds(SC_TIME-(end-start));
+    break;
+
+    case 1:
+    start = micros();
+    gas_request();
+    speed_request();
+    brake_request();
+    
+    end = micros();
+    delayMicroseconds(SC_TIME-(end-start));
+    break;
+    
+    case 2:
+    start = micros();
+    mixer_request();
+    
+    end = micros();
+    delayMicroseconds(SC_TIME-(end-start));
+    break;
+
+    case 3:
+    start = micros();
+    slope_request();
+    
+    end = micros();
+    delayMicroseconds(SC_TIME-(end-start));
+    break;
+
+    case 4:
+    start = micros();
+    gas_request();    
+    speed_request();
+    brake_request();
+    mixer_request();    
+    
+    end = micros();
+    delayMicroseconds(SC_TIME-(end-start));
+    break;    
+  } 
+  sc = sc + 1;
+}
+
+// --------------------------------
+// AUXILIARY FUNCTIONS
+// --------------------------------
+
+
+
+// --------------------------------------
+// Handler function: receiveEvent
+// --------------------------------------
+void receiveEvent(int num)
+{
+   char aux_str[MESSAGE_SIZE+1];
+   int i=0;
+
+   // read message char by char, up to MESSAGE_SIZE
+   for (int j=0; j<num; j++) {
+      char c = Wire.read();
+      if (i < MESSAGE_SIZE) {
+         aux_str[i] = c;
+         i++;
+      }
+   }
+   aux_str[i]='\0';
+
+   // if message is correct, load it
+   if ((num == MESSAGE_SIZE) && (!request_received)) {
+      memcpy(request, aux_str, MESSAGE_SIZE+1);
+      request_received = true;
+   }
+}
+
+// --------------------------------------
+// Handler function: requestEvent
+// --------------------------------------
+void requestEvent()
+{
+   // if there is an answer send it, else error
+   if (answer_requested) {
+      Wire.write(response,MESSAGE_SIZE);
+      memset(response,'\0', MESSAGE_SIZE+1);
+   } else {
+      Wire.write("NOT REDY",MESSAGE_SIZE);
+   }
+  
+   // set answer empty
+   request_received = false;
+   answer_requested = false;
+   memset(request,'\0', MESSAGE_SIZE+1);
+   memset(response,'0', MESSAGE_SIZE);
+}
+
+// ------------------------------------
+// Resets the buffers in order to 
+//   continue with the communication
+// ------------------------------------
+int resetBuffer(){
+   // set buffers and flags
+      memset(request,'\0', MESSAGE_SIZE+1);
+      request_received = false;
+      answer_requested = true;
+}
+
+int slope_request(){
+   
+//update the slope value
+   int errno = readSlope();
+  //Process a SLOPE request
+  if((request_received) &&
+        (0 == strcmp("SLP: REQ",request)) ){    
+ 
+    switch(errno){
+      case -1:
+         sprintf(response, UP_SLP_MSG);       
+         break;
+      case 0:
+         sprintf(response, FLAT_SLP_MSG);
+         break;
+      case 1:
+         sprintf(response, DOWN_SLP_MSG);
+         break;
+       default:
+        sprintf(response, ERROR_MSG);  
+    }
+          
+    resetBuffer();
+    return 0;
+  }
+}
+
+int speed_request(){
+   int errno = computeSpeed();
+    //Process a SPEED request
+  if ((request_received) &&
+        (0 == strcmp("SPD: REQ",request)) ){        
+    char num_str[5];
+      dtostrf(speed,4,1,num_str);      
+   sprintf(response, SPD_MSG, num_str);
+   resetBuffer();  
+  }
+  
+  return 0;
+}
+
+
+int gas_request(){
+   int errno = 0;
+   if (request_received && 0 == strcmp("GAS: SET",request)){	      
+     errno = switchGas(SET);
+     //Process a GAS request
+      if (errno == 0){
+         sprintf(response, ERROR_MSG);
+      }else{
+         sprintf(response, OK_MSG, "GAS");
+      }      
+      resetBuffer();      
+   }else if (request_received && (0 == strcmp("GAS: CLR",request))){
+     errno = switchGas(CLR);
+     //Process a GAS request
+      if (errno == 0){
+         sprintf(response, ERROR_MSG);
+      }else{
+         sprintf(response, OK_MSG, "GAS");
+      }
+     resetBuffer();      
+   }
+   return 0;  
+}
+
+int brake_request(){   
+  int errno = 0;
+   if (request_received && 0 == strcmp("BRK: SET",request)){
+
+      errno = switchBrake(SET);
+     //Process a BRK request
+      if (errno == 0){
+         sprintf(response, ERROR_MSG);
+      }else{
+         sprintf(response, OK_MSG, "BRK");
+      }
+      resetBuffer();
+      
+   }else if (request_received && 0 == strcmp("BRK: CLR",request)){
+   	errno = switchBrake(CLR);
+     //Process a BRK request
+      if (errno == 0){
+         sprintf(response, ERROR_MSG);
+      }else{
+         sprintf(response, OK_MSG, "BRK");
+      }
+      resetBuffer();
+   }
+   return 0;  
+}
+
+int mixer_request(){
+	int errno = 0;
+   if (request_received && 0 == strcmp("MIX: SET",request)){
+	errno = switchMixer(SET);
+      //Process a MIX request
+      if (errno == 0){
+         sprintf(response, ERROR_MSG);
+      }else{
+         sprintf(response, OK_MSG, "MIX");
+      }
+      resetBuffer();      
+      
+   }else if(request_received && 0 == strcmp("MIX: CLR",request)){
+   	errno = switchMixer(CLR);
+     //Process a MIX request
+      if (errno == 0){
+         sprintf(response, ERROR_MSG);
+      }else{
+         sprintf(response, OK_MSG, "MIX");
+      }
+      resetBuffer();      
+     
+   }
+   return 0;
+}
+
+
+// --------------------------------------
+// Function: computeSpeed
+//	Definition: Computes the current speed, always 0 <= speed 
+//	Returns: 1 if successful. 0 otherwise
+// --------------------------------------
+int computeSpeed(){
+  float acceleration = slope*INERTIA + gas_status*GAS_POWER + brake_status*BRAKE_POWER;
+  //speed = speed_0 + acceleration*time; 
+  
+    
+  speed += acceleration/10;  
+  //Map input value [0, 70] into output range [0, 255]  
+  analogWrite(SPD_PIN_OUT, ((speed/MAX_SPEED)*255));  
+  return 1;
+
+}
+
+
+// --------------------------------------
+// Function: readSlope
+//	Definition: Read the slope sensor to obtain the current slope value
+//	Returns: The value of the slope. 1 upwards, 0 flat, -1 downwards
+// --------------------------------------
+int readSlope(){
+	/*
+  	pin1 	pin2	SLP
+  	0 		0 		-1
+    1 		0 		0
+    0 		1 		0
+    1 		1 		1
+  */
+	int base_value = -1;
+	int pin1 = digitalRead(SLP_PIN_IN_1);
+	int pin2 = digitalRead(SLP_PIN_IN_2);  	
+  	slope = base_value + pin1 + pin2;    	
+  	return slope;
+}
+
+
+// --------------------------------------
+// Function: switchMixer
+//	Definition: Turns on/off the mixer based on the request
+//	Returns: 1 if the operation was sucessful. 0 otherwise
+// --------------------------------------
+
+int switchMixer(int status){
+  int errno = 1;
+ 
+  switch(status){
+    case SET:    
+      mixer_status = 1;     
+      break;
+    
+    case CLR:    
+      mixer_status = 0;
+      break;
+    default:
+      //The status value is invalid
+      errno = 0;
+  }
+	//Display the new status of the mixer
+  digitalWrite(MIX_PIN_OUT, mixer_status);
+  return errno;
+}
+// --------------------------------------
+// Function: switchAccelerator
+//	Definition: Turns on/off the accelerator based on the request
+//	Returns: 1 if the operation was sucessful. 0 otherwise
+// --------------------------------------
+int switchGas(int status){
+  int errno = 1;
+  
+  switch(status){
+    case SET:    	
+      gas_status = 1;     
+      break;
+    
+    case CLR:   
+      gas_status = 0;
+      break;
+    default:
+      //The status value is invalid
+      errno = 0;
+  }
+	//Display the new status of the accelerator
+  digitalWrite(GAS_PIN_OUT, gas_status);
+  return errno;
+}
+// --------------------------------------
+// Function: switchBrake
+//	Definition: Turns on/off the brake based on the request
+//	Returns: 1 if the operation was sucessful. 0 otherwise
+// --------------------------------------
+int switchBrake(int status){
+
+  int errno = 1;
+  switch(status){
+    case SET:
+      brake_status = 1;
+      break;
+    case CLR:
+      brake_status = 0;    
+      break;
+    default:
+      //The status value is invalid
+      errno = 0;
+  }
+	//Display the new status of the accelerator
+  digitalWrite(BRK_PIN_OUT, brake_status);
+  return errno;
+}
